660283170325e21d5e30abf47feb961ab9ef36f3
Tunes <168590642+tuninhoXP@users.noreply.github.com>
2025-10-31 12:47:08 -0300

    Add files via upload

---
+++ salario.py
@@ -0,0 +1,67 @@
+
+import time
+loop = True
+
+def calcularsalario():
+    global loop
+    salario = float(input("DIGITE SEU SALÁRIO: "))
+    horas = float(input("DIGITE HORAS TRABALHADAS: "))
+    sh = salario / horas
+    print("VOCE RECEBE ", sh, " R$ POR HORA")
+    time.sleep(1)
+    finalizar = int(input("DESEJA FINALIZAR O PROGRAMA? 1 = SIM"))
+    if finalizar == 1:
+        loop = False
+        return loop
+    return sh
+
+
+def calcularfaltas():
+    atrasos = int(input("QUANTAS FALTAS VOCÊ TEM?"))
+    global loop
+    if atrasos >= 3:
+        print("Você está suspenso")
+    elif atrasos == 2:
+        print("Mais um e você estará suspenso")
+    elif atrasos == 1:
+        print("Vou deixar passar pois é a primeira vez")
+    else:
+        print("Ok! pode entrar")
+    time.sleep(1)
+    finalizar = int(input("DESEJA FINALIZAR O PROGRAMA? 1 = SIM"))
+    if finalizar == 1:
+        loop = False
+        return loop
+
+def calcularLiberade():
+    disponivel = input("Você está livre? S/N")
+    global loop
+    if disponivel == "S" or "s":
+        print("Estou sim")
+    else:
+        print("Não estou")
+        time.sleep(1)
+    finalizar = int(input("DESEJA FINALIZAR O PROGRAMA? 1 = SIM"))
+    if finalizar == 1:
+        loop = False
+        return loop
+
+while loop == True:
+
+    print("-----------------------------------------------")
+    print("Bem vindo aos meus programas!")
+    time.sleep(2)
+    selecionarprograma = int(
+        input("1 = CALCULAR SALARIO / 2 = CALCULAR FALTAS  / 3 = CALCULARLIBERDADE  "))
+    if selecionarprograma == 1:
+        calcularsalario()
+    elif selecionarprograma == 2:
+        calcularfaltas()
+    elif selecionarprograma == 3:
+        calcularLiberade()
+    else: print("RESPOSTA INVÁLIDA")
+
+
+
+
+
